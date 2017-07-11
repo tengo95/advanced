1bf2f00ed4f81f0e303aa3c6c7af37b2dbb21eed
Ryan Desmond <radesmond@gmail.com>
2017-07-11 16:54:35 +0700

    changing package name

---
+++ src/main/java/string_arrays/StringReverse.java
@@ -0,0 +1,18 @@
+package string_arrays;
+
+public class StringReverse {
+
+    /*
+    This application must be called from the command line
+    You must pass a single String into the class from the command line
+    The main() must print out the contents of the String reverse order
+     */
+	public static void main(String[] args){
+
+	    // initialize the string from the args array
+
+        // print out the letters in reverse order
+
+	}
+
+}
