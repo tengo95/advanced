134a1d38b568b5d7d0751e899fd9251e15b01514
Ryan Desmond <radesmond@gmail.com>
2017-07-12 19:49:01 +0700

    adding sudoku challenge and resources

---
+++ src/main/java/strings_arrays/SuDoku.java
@@ -0,0 +1,28 @@
+package strings_arrays;
+
+/*
+Su Doku (Japanese meaning number place) is the name given to a popular puzzle concept. Its origin is unclear,
+but credit must be attributed to Leonhard Euler who invented a similar, and much more difficult, puzzle idea called
+Latin Squares.
+
+The objective of Su Doku puzzles, however, is to replace the blanks (or zeros) in a 9 by 9 grid in
+such that each row, column, and 3 by 3 box contains each of the digits 1 to 9. The example shown in sudoku.ong in the
+ 'resources' folder in the this project shows a typical starting puzzle grid and its solution grid.
+
+A well constructed Su Doku puzzle has a unique solution and can be solved by logic, although it may be necessary to
+employ "guess and test" methods in order to eliminate options (there is much contested opinion over this). The
+complexity of the search determines the difficulty of the puzzle; the example above is considered easy because it can
+be solved by straight forward direct deduction.
+
+The text file, found in the 'resources' directory of this project, contains fifty different Su Doku puzzles
+ranging in difficulty, but all with unique solutions (the first puzzle in the file is the example shown in sudoku.png
+found in the resources directory as well).
+
+By solving all fifty puzzles find the sum of the 3-digit numbers found in the top left corner of each solution grid;
+for example, 483 is the 3-digit number found in the top left corner of the solution grid above.
+
+*/
+
+public class SuDoku {
+
+}
